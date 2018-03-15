8d067467df5f4e7361368fc40cf4758da48fda82
Flesh12138 <1577760218@qq.com>
2018-03-15 17:56:12 +0800

    有了github再也不用以日期命名了。。
    3-15最后是在17：54编辑完毕
    evaluate改名evaluate_exp
    下一步：
    把evaluate当中简单的如-t交给另外一个函数去完成，evaluate负责识别函数并创建实例
    另外那个叫evaluate_simple_exp

--- try.py
+++ try.py
@@ -1,4 +1,4 @@
-# def get_bracket_position(exp):
+﻿# def get_bracket_position(exp):
 # 	bracket_position=[]
 # 	left_now=0
 # 	right_now=0
@@ -17,5 +17,17 @@ def get_bracket_position(exp):
 			left_bracket_position.append(char_pos)
 		if exp[char_pos]==')':
 			result.append([left_bracket_position.pop(),char_pos])
+	assert len(left_bracket_position)==0
 	return result
-print(get_bracket_position('(((1+1)*2)*4)'))
+
+def op_in_bracket(op_position,bracket_positions):
+    """
+	接受来自get_bracket position的数组，判断运算符是否在括号内
+	格式：[[2,4],[1,6]]
+	"""
+    result=False
+    for i in bracket_positions: #TODO 确认op不是括号
+        if i[0]<op_position<i[1]:
+            result=True
+    return result
+print(op_in_bracket(4,get_bracket_position('(((1+1)*2)*4)')))
